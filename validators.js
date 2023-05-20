c0ed999cd68d1ebac9796c41de96103a7fcbeb32
ChaCharrier <charlotte.charrier97@gmail.com>
2023-05-20 15:21:18 +0200

    fix quest 04bis

--- validators.js
+++ validators.js
@@ -1,25 +1,34 @@
 const validateMovie = (req,res, next) => {
     const { title, director, year, color, duration } = req.body;
     const errors = [];
+    const durationRegex = /[0-9]/;
   
-    if (title == null) {
+    if (title === null) {
       errors.push({ field: "title", message: "This field is required" });
     } else if (title.length >= 255) {
         errors.push({ field: "title", message: "Should contain less than 255 characters" });
     }
-    if (director == null) {
+    if (director === null) {
       errors.push({ field: "director", message: "This field is required" });
-    }
-    if (year == null) {
+    } else if (director.length >= 255) {
+      errors.push({ field: "director", message: "Should contain less than 255 characters" });
+  }
+    if (year === null) {
       errors.push({ field: "year", message: "This field is required" });
-    }
-    if (color == null) {
+    } else if (year.length >= 255) {
+      errors.push({ field: "year", message: "Should contain less than 255 characters" });
+  }
+    if (color === null) {
       errors.push({ field: "color", message: "This field is required" });
-    }
-    if (duration == null) {
+    } else if (color.length >= 255) {
+      errors.push({ field: "color", message: "Should contain less than 255 characters" });
+  }
+    if (duration === null) {
       errors.push({ field: "duration", message: "This field is required" });
+    } else if (!durationRegex.test(duration)) {
+      errors.push({ field: "duration", message: "Invalid duration" });
     }
-   
+
     if (errors.length) {
       res.status(422).json({ validationErrors: errors });
     } else {
@@ -32,12 +41,12 @@ const validateUser = (req,res, next) => {
     const errors = [];
     const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
 
-    if(firstname == null) {
+    if(firstname === null) {
         errors.push({field :"firstname", message: "This field is required"});
     } else if (firstname.length >= 255) {
         errors.push({field: "firstname", message: "Should contain less than 255 characters"});
     }
-    if(lastname == null) {
+    if(lastname === null) {
         errors.push({field :"lastname", message: "This field is required"});
     } else if (lastname.length >= 255) {
         errors.push({field: "lastname", message: "Should contain less than 255 characters"});
@@ -47,12 +56,12 @@ const validateUser = (req,res, next) => {
     } else if (email.length >= 255) {
         errors.push({field : "email", message : "Should contain less than 255 characters"});
     }
-    if(city == null) {
+    if(city === null) {
         errors.push({field :"city", message: "This field is required"});
     } else if (city.length >= 255) {
         errors.push({field: "city", message: "Should contain less than 255 characters"});
     }
-    if(language == null) {
+    if(language === null) {
         errors.push({field :"language", message: "This field is required"});
     } else if (language.length >= 255) {
         errors.push({field: "language", message: "Should contain less than 255 characters"});
