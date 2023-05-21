6ce9be3d3199e410365c6d4325ba9ecb671128a2
ChaCharrier <charlotte.charrier97@gmail.com>
2023-05-21 17:50:56 +0200

    fix quest 04bis

--- validators.js
+++ validators.js
@@ -5,23 +5,25 @@ const validateMovie = (req,res, next) => {
   
     if (title === null) {
       errors.push({ field: "title", message: "This field is required" });
-    } else if (title.length >= 255) {
+    } else if (title.length > 255) {
         errors.push({ field: "title", message: "Should contain less than 255 characters" });
-    }
+      }
     if (director === null) {
       errors.push({ field: "director", message: "This field is required" });
-    } else if (director.length >= 255) {
+    } else if (director.length > 255) {
       errors.push({ field: "director", message: "Should contain less than 255 characters" });
-  }
+    }
     if (year === null) {
       errors.push({ field: "year", message: "This field is required" });
-    } else if (year.length >= 255) {
+    } else if (year.length > 255) {
       errors.push({ field: "year", message: "Should contain less than 255 characters" });
   }
     if (color === null) {
       errors.push({ field: "color", message: "This field is required" });
-    } else if (color.length >= 255) {
+    } else if (color.length > 255) {
       errors.push({ field: "color", message: "Should contain less than 255 characters" });
+  } else if (color != "1" && color != "0") {
+    errors.push({ field: "color", message: "Should contain 0 or 1"});
   }
     if (duration === null) {
       errors.push({ field: "duration", message: "This field is required" });
@@ -43,27 +45,27 @@ const validateUser = (req,res, next) => {
 
     if(firstname === null) {
         errors.push({field :"firstname", message: "This field is required"});
-    } else if (firstname.length >= 255) {
+    } else if (firstname.length > 255) {
         errors.push({field: "firstname", message: "Should contain less than 255 characters"});
     }
     if(lastname === null) {
         errors.push({field :"lastname", message: "This field is required"});
-    } else if (lastname.length >= 255) {
+    } else if (lastname.length > 255) {
         errors.push({field: "lastname", message: "Should contain less than 255 characters"});
     }
     if (!emailRegex.test(email)) {
         errors.push({ field: "email", message: "Invalid email" });
-    } else if (email.length >= 255) {
+    } else if (email.length > 255) {
         errors.push({field : "email", message : "Should contain less than 255 characters"});
     }
     if(city === null) {
         errors.push({field :"city", message: "This field is required"});
-    } else if (city.length >= 255) {
+    } else if (city.length > 255) {
         errors.push({field: "city", message: "Should contain less than 255 characters"});
     }
     if(language === null) {
         errors.push({field :"language", message: "This field is required"});
-    } else if (language.length >= 255) {
+    } else if (language.length > 255) {
         errors.push({field: "language", message: "Should contain less than 255 characters"});
     }
 
